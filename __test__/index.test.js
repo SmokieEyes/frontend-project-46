139c351bc2c6008868be290abcb1b1d016aabc1b
LucyMiMi <lucymur.dev@gmail.com>
2023-02-13 14:52:41 +0300

    Step 5

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,6 +1,11 @@
 import gendiff from '../src/index.js';
 import resultJSON from '../__fixtures__/resultJSON.js';
+import resultYAML from '../__fixtures__/resultYAML.js';
 
 test('JSON json test', () => {
   expect(gendiff('./__fixtures__/file1.json', './__fixtures__/file2.json')).toEqual(resultJSON);
+});
+
+test('YAML yaml test', () => {
+  expect(gendiff('./__fixtures__/file1.yml', './__fixtures__/file2.yml')).toEqual(resultYAML);
 });
--- src/parsers.js
+++ src/parsers.js
@@ -1,5 +1,12 @@
 import { readFileSync } from 'node:fs';
+import { extname } from 'node:path';
+import yaml from 'js-yaml';
 
-const parsers = (filepath) => JSON.parse(readFileSync(filepath, 'utf-8'));
+const parsers = (filepath) => {
+  if (extname(filepath) === '.yaml' || extname(filepath) === '.yml') {
+    return yaml.load(readFileSync(filepath, 'utf-8'));
+  }
+  return JSON.parse(readFileSync(filepath, 'utf-8'));
+};
 
-export default parsers;
+export default parsers ;
