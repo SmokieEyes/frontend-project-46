d819dcd41176c558e49cab82930defaa759c0e85
LucyMiMi <lucymur.dev@gmail.com>
2023-03-05 15:07:54 +0300

    refactoring

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,48 +1,42 @@
 import _ from 'lodash';
 
-const indent = ' ';
-const indentSize = 4;
-const forBracket = 2;
-const curIndent = (depth) => indent.repeat(indentSize * depth - forBracket);
-const bracketIndent = (depth) => indent.repeat(indentSize * depth - indentSize);
-const buildStyleTree = (lines, depth) => ['{', ...lines, `${bracketIndent(depth)}}`].join('\n');
-const childProcess = (space, key, value) => `${space}  ${key}: ${value}`;
-const checkObj = (data, depth) => {
-  if (!_.isObject(data)) return data;
-  const requiredIndent = curIndent(depth);
-  const lines = _.keys(data).map((key) => {
-    const checkingDeeper = checkObj(data[key], depth + 1);
-    return childProcess(requiredIndent, key, checkingDeeper);
-  });
-  return buildStyleTree(lines, depth);
-};
+const stylishTree = (dataTree, deepness = 1, indent = ' ', indentSize = 4, indentLen = 2) => {
+  const currentIndent = (depth) => indent.repeat(indentSize * depth - indentLen);
+  const bracketIndent = (depth) => indent.repeat(indentSize * depth - indentSize);
+  const buildStyleTree = (lines, depth) => ['{', ...lines, `${bracketIndent(depth)}}`].join('\n');
+  const checkObj = (data, depth) => {
+    if (!_.isObject(data)) return data;
+    const lines = _.keys(data).map((key) => `${currentIndent(depth)}  ${key}: ${checkObj(data[key], depth + 1)}`);
+    return buildStyleTree(lines, depth);
+  };
 
-const actionBasedOnStatus = (value, depth) => {
-  const setStringLines = (mark, name, val, dep) => `${curIndent(dep)}${mark} ${name.name}: ${checkObj(val, dep + 1)}`;
-  if (value.status === 'added') {
-    return setStringLines('+', value, value.value, depth);
-  }
-  if (value.status === 'removed') {
-    return setStringLines('-', value, value.value, depth);
-  }
-  if (value.status === 'changed') {
-    return [
-      setStringLines('-', value, value.oldValue, depth),
-      setStringLines('+', value, value.newValue, depth),
-    ];
-  }
-  return setStringLines(indent, value, value.value, depth);
-};
+  const actionBasedOnStatus = (key, depth) => {
+    const setStringLines = (mark, name, val, dep) => `${currentIndent(dep)}${mark} ${name.name}: ${checkObj(val, dep + 1)}`;
+    if (key.status === 'added') {
+      return setStringLines('+', key, key.value, depth);
+    }
+    if (key.status === 'removed') {
+      return setStringLines('-', key, key.value, depth);
+    }
+    if (key.status === 'changed') {
+      return [
+        setStringLines('-', key, key.oldValue, depth),
+        setStringLines('+', key, key.newValue, depth),
+      ];
+    }
+    return setStringLines(indent, key, key.value, depth);
+  };
 
-const formsATree = (value, depth) => {
-  if (value.status === 'root') {
-    return buildStyleTree(value.children.flatMap((child) => formsATree(child, depth)), depth);
-  }
-  if (value.status === 'nested') {
-    const checkingDeeper = value.children.flatMap((child) => formsATree(child, depth + 1));
-    const requiredIndent = curIndent(depth);
-    return childProcess(requiredIndent, value.name, buildStyleTree(checkingDeeper, depth + 1));
-  }
-  return actionBasedOnStatus(value, depth);
+  const buildTree = (value, depth) => {
+    if (value.status === 'root') {
+      return buildStyleTree(value.children.flatMap((child) => buildTree(child, depth)), depth);
+    }
+    if (value.status === 'nested') {
+      const checkingDeeper = value.children.flatMap((child) => buildTree(child, depth + 1));
+      return `${currentIndent(depth)}  ${value.name}: ${buildStyleTree(checkingDeeper, depth + 1)}`;
+    }
+    return actionBasedOnStatus(value, depth);
+  };
+  return buildTree(dataTree, deepness);
 };
-export default (data) => formsATree(data, 1);
+export default stylishTree;
